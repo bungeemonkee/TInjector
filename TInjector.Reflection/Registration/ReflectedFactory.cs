0ce3ebb8e4ff2c6f51790293c21f74230223a346
David Love <bungeemonkee@gmail.com>
2016-03-25 11:57:28 -0400

    A potentially working version of the reflection extension library

---
+++ TInjector.Reflection/Registration/ReflectedFactory.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace TInjector.Reflection.Registration
+{
+    public class ReflectedFactory<T> : IFactory<T>
+        where T : class
+    {
+        private Func<IRequest, T> _constructorInvoker;
+
+        public T Make(IRequest request)
+        {
+            if (_constructorInvoker == null)
+            {
+                _constructorInvoker = GetConstructorInvoker(request);
+            }
+
+            return _constructorInvoker(request);
+        }
+
+        protected virtual ConstructorInfo SelectConstructor(Func<Type, bool> isRegistered)
+        {
+            // Get the public constructor where all parameters are registered that has the most parameters
+            return typeof(T)
+                .GetConstructors(BindingFlags.Public)
+                .Select(x => new
+                {
+                    Constructor = x,
+                    Parameters = x.GetParameters()
+                })
+                .Select(x => new
+                {
+                    Constructor = x.Constructor,
+                    Parameters = x.Parameters,
+                    RegisteredParametersCount = x.Parameters.Count(y => isRegistered(y.ParameterType))
+                })
+                .Where(x => x.Parameters.Length == x.RegisteredParametersCount)
+                .OrderByDescending(x => x.RegisteredParametersCount)
+                .Select(x => x.Constructor)
+                .First();
+        }
+
+        private Func<IRequest, T> GetConstructorInvoker(IRequest request)
+        {
+            if (_constructorInvoker != null) return _constructorInvoker;
+
+            var constructor = SelectConstructor(x => request.GetRegistration(x) != null);
+            var parameters = constructor.GetParameters().Select(x => x.ParameterType).ToArray();
+
+            return (IRequest r) =>
+            {
+                var arguments = parameters.Select(x => r.Get(x)).ToArray();
+                return (T)constructor.Invoke(arguments);
+            };
+        }
+    }
+}
---
+++ TInjector.Reflection/Registration/ReflectedRegistration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using TInjector.Registration;
+
+namespace TInjector.Reflection.Registration
+{
+    public class ReflectedRegistration<T> : FluentRegistration<T>
+        where T : class
+    {
+        protected static readonly Type[] ServicesToIgnore =
+        {
+            typeof(IDisposable),
+            typeof(IComponent)
+        };
+
+        protected static readonly TypeFilter TypeFilter = (t, o) => !ServicesToIgnore.Contains(t);
+
+        public ReflectedRegistration()
+            : this(new ReflectedFactory<T>())
+        { }
+
+        public ReflectedRegistration(Expression<Func<IRequest, T>> expression)
+            : this(new ExpressionFactory<T>(expression))
+        { }
+
+        public ReflectedRegistration(IFactory<T> factory)
+            : base(factory)
+        {
+            var services = Implementer
+                .FindInterfaces(TypeFilter, null)
+                .Where(x => !ServicesToIgnore.Contains(x))
+                .ToArray();
+
+            AddServices(services);
+        }
+    }
+}
